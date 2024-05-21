a76ee4fa6200e3c9871624891e7ff1602ead34d9
jaccong <151510379+jaccong@users.noreply.github.com>
2024-05-21 10:53:59 +0800

    Update all.py

--- all.py
+++ all.py
@@ -1,3 +1,13 @@
-with open('9.txt', 'r', encoding='utf-8') as file:
-  txt = file.read()
-  print(f"读取文件9.txt:{txt}")
+import requests
+
+try:
+  x = requests.get('https://raw.githubusercontent.com/jaccong/loong/main/x.m3u',timeout=1).text
+except Exception as e:
+    print(f'error:【{e}】')
+
+with open('lity.txt', 'r', encoding='utf-8') as file:
+  lity = file.read()
+
+with open("all.txt", 'w', encoding='utf-8') as file:
+  file.write(f'{x}\n')
+  file.write(f'{lity}\n')
